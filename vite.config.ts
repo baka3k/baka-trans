cd2575fa312a43a6f662d61113409a09a6ad779e
Le Quang Hiep <baka3k@gmail.com>
2026-07-08 15:33:31 +0700

    fix: update development server port to 11111 in configuration files

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   clearScreen: false,
   server: {
-    port: 5173,
+    port: 11111,
     strictPort: true,
     watch: {
       ignored: ["**/src-tauri/**"],
